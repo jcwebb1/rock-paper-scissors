db821bf5f5b0d4c1e6bc9ea1d1bf7f19c150d6f6
jaidwebb <webbjaid@gmail.com>
2024-01-09 16:57:07 -0600

    added getPlayerChoice function

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const choices = ["rock","paper","scissors"]
 
+
+function getPlayerChoice() {
+    let input = prompt("Type Rock, Paper, or Scissors");
+    while (input == null) {
+        input = prompt("Type Rock, Paper, or Scissors")
+    }
+    input = input.toLowerCase();
+    console.log(input)
+}
+
 //to get the computer to return a random choice
 //Math.floor rounds down to nearest decimal
 function getComputerChoice() {
